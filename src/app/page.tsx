67591b71aa8155242cc10152007906c4a3d48d7c
Vladislav <vldslvvasilevic@gmail.com>
2025-04-18 17:06:24 +0200

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Home from "@/components/main/home";
 import About from "@/components/main/about";
 import Contacts from "@/components/main/contacts";
 import Gallery from "@/components/main/gallery";
